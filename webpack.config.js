e918f86c0cd04d06e70c2996d2089f9ae76c913e
Dhafer <soprano1993@live.fr>
2019-12-10 15:16:43 +0100

    fix(*) : added build/release content to dist folder + cleanup webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -8,25 +8,19 @@ const CopyPlugin = require('copy-webpack-plugin')
 module.exports = async env => {
     return {
         entry: {
-            main: './src/main.ts'
+            renderGraphImage: './src/main.ts'
         },
         output: {
-            // filename: './[name].js',
-            filename: './renderGraphImage.js',
+            filename: './[name].js',
             path: path.resolve(__dirname, 'dist')
         },
 
-        // plugins: [
-        //     new CopyPlugin([{
-        //         from: 'node_modules/canvas/build/Release/canvas.node',
-        //         to: './',
-        //         // transformPath(targetPath, absolutePath) {
-        //         //     console.log("path : " , absolutePath)
-        //         //     return Promise.resolve(absolutePath);
-        //         // },
-        //     }])
-        // ],
-        // externals: './canvas',
+        plugins: [
+            new CopyPlugin([{
+                from: 'node_modules/canvas/build/Release',
+                to: './'
+            }])
+        ],
         target: 'node',
         node: {
             __dirname: false
@@ -39,21 +33,7 @@ module.exports = async env => {
                 {
                     test: /\.node$/,
                     loader: 'native-ext-loader', 
-                    // options : {
-                    //     rewritePath: path.resolve(__dirname, 'dist')
-                    // }
                 },
-                // {
-                //     test: /\.node$/,
-                //     use: 'native-ext-loader'
-                // },
-                // {
-                //     test: /\.node$/,
-                //     loader: 'file-loader' ,
-                //     options : {
-                //         name : '[name].[ext]'
-                //     }
-                // },
                 {
                     test: /\.tsx?$/,
                     use: [
@@ -79,33 +59,33 @@ module.exports = async env => {
             modules: [path.resolve('./node_modules')],
             extensions: ['.ts', '.js' ]
         },
-        // optimization: {
-        //     minimize: true,
-        //     minimizer: [
-        //         new TerserPlugin({
-        //             terserOptions: {
-        //                 parse: {
-        //                     ecma: 8
-        //                 },
-        //                 compress: {
-        //                     ecma: 5,
-        //                     warnings: false,
-        //                     comparisons: false,
-        //                     inline: 2
-        //                 },
-        //                 mangle: {
-        //                     safari10: true
-        //                 },
-        //                 output: {
-        //                     ecma: 5,
-        //                     comments: false,
-        //                     ascii_only: true
-        //                 }
-        //             },
-        //             parallel: true,
-        //             cache: true
-        //         })
-        //     ]
-        // }
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new TerserPlugin({
+                    terserOptions: {
+                        parse: {
+                            ecma: 8
+                        },
+                        compress: {
+                            ecma: 5,
+                            warnings: false,
+                            comparisons: false,
+                            inline: 2
+                        },
+                        mangle: {
+                            safari10: true
+                        },
+                        output: {
+                            ecma: 5,
+                            comments: false,
+                            ascii_only: true
+                        }
+                    },
+                    parallel: true,
+                    cache: true
+                })
+            ]
+        }
     }
 }
